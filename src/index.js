7ce0a1c15e25f5a9b80a5ae7382de5f44ebf59e9
Herbrand <Herbrand.Hofker@gmail.com>
2021-03-03 10:19:57 +0100

    a

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import './city.mjs'
 
 const spaMenu = document.querySelector("spa-menu");
 
-spaMenu.switch=(menuItem)=> {
-  spaMenu.items.forEach((item) =>item.el.style.display = "none" )
+spaMenu.switch = (menuItem) => {
+  spaMenu.items.forEach((item) => item.el.style.display = "none")
   menuItem.el.style.display = "block";
 };
 
+spaMenu.items = addItems();
 
-for (const el of document.getElementsByClassName("tabcontent")) {
-  spaMenu.items.push(new MenuItem(el.id, el))
+function addItems() {
+  const items = [];
+  for (const el of document.getElementsByClassName("tabcontent")) {
+    items.push(new MenuItem(el.id, el))
+  }
+  return items;
 }
 
 const dialog = document.querySelector("login-dialog");
